d8017f915888c35c4d216c12996627c49c65c48b
agomemart <agomemart@ieschandomonte.edu.es>
2024-11-13 14:27:47 +0100

    v2.3.9

---
+++ src/ud2/ejercicios/E0410.java
@@ -0,0 +1,14 @@
+package ud2.ejercicios;
+
+public class E0410 {
+    public static double elevarCuadrado(double a, int n) {
+        double resultado = 0;
+        for (int i = 0; i < n; i++) {
+            resultado = a * a;
+        }
+        return resultado;
+    }
+    public static void main(String[] args) {
+        System.out.println(elevarCuadrado(2, 3));
+    }
+}
---
+++ src/ud2/ejercicios/E0412.java
@@ -0,0 +1,23 @@
+package ud2.ejercicios;
+/*
+ *  Diseñar una función recursiva que calcule el enésimo término de la serie de
+Fibonacci. En esta serie el enésimo valor se calcula sumando los dos valores anteriores
+de la serie, es decir:
+fibonacci(n) = fibonacci(n-1) + fibonacci(n-2)
+fibonacci(1) = 1
+fibonacci(0) = 1
+ */
+public class E0412 {
+    public static int fibonacci(int n) {
+        if (n == 0) {
+            return 1;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return fibonacci(n-1) + fibonacci(n-2);
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(fibonacci(11));
+    }
+}
