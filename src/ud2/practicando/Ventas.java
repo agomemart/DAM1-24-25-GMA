b4a150552b63ac9d9c46396e51d1fad846c03d23
agomemart <agomemart@ieschandomonte.edu.es>
2024-12-11 20:48:42 +0100

    v.3.0.3

--- src/ud2/practicando/Ventas.java
+++ src/ud2/practicando/Ventas.java
@@ -1,46 +1,60 @@
 package ud2.practicando;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Ventas {
-    public static String diaSemana(int dia) {
-        return switch (dia){
-            case 0: yield "MARTES";
-            case 1: yield "MIERCOLES";
-            case 2: yield "JUEVES";
-            case 3: yield "VIERNES";
-            case 4: yield "SABADO";
-            case 5: yield "DOMINGO";
-            default: yield "ERROR";
-        }
-    }
     public static void main(String[] args) {
-        double max = 0, min = 0, media = 0;
-        int diaMax, diaMin;
-        double recaudacion;
+        double venta;
+        char categoria;
+        double desayuno = 0;
+        double comida = 0;
+        double merienda = 0;
+        double cena = 0;
+        double copa = 0;
+        int contadorComidas = 0;
+
         Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < 6; i++) {
-            recaudacion = sc.nextDouble();
-        if (i == 0) {
-            min = recaudacion;
-            max = recaudacion;
-            diaMin = 0;
-            diaMax = 0;
-        } else {
-            if (recaudacion < min) {
-                min = recaudacion;
-                diaMin = i;
-            } else if (recaudacion > max) {
-                max = recaudacion;
-                diaMax = i;
+        try {
+            do {
+                System.out.print("Inserta el importe de la venta: ");
+                venta = sc.nextDouble();
+                sc.nextLine();
+                System.out.print("Elije entre D (desayuno), A (comida), M (merienda), I (Cena) y C (copas): ");
+                categoria = sc.nextLine().toUpperCase().charAt(0);
+                    switch (categoria) {
+                        case 'D':
+                            desayuno = desayuno += venta;
+                            break;
+                        case 'A':
+                            comida = comida += venta;
+                            contadorComidas++;
+                            break;
+                        case 'M':
+                            merienda = merienda += venta;
+                            break;
+                        case 'I':
+                            cena = cena += venta;
+                            break;
+                        case 'C':
+                            copa = copa += venta;
+                            break;
+                        default:
+                            System.out.println("Error, caracter insertado no válido.");
+                            break;
+                    }
+            } while (categoria != 'N');
+            double mediaTotalDia = (desayuno + comida + merienda + cena + copa) / 5;
+            double mediaComidas = (comida / contadorComidas);
+            if (mediaTotalDia > mediaComidas) {
+                System.out.println("SI");
+            } else {
+                System.out.println("NO");
             }
+            
+        } catch (InputMismatchException e) {
+            System.out.println("Error: dato en formato incorrecto");
         }
-            media += recaudacion;
-        }
-
-        media /= 6;
-
-        System.out.println(diaSemana(diaMax) + "" + diaSemana(diaMin) + recaudacion > media ? "SI" : "NO");
+        sc.close();
     }
-
 }
