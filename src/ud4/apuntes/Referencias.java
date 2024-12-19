820f060cb2db10327ec66c03aad4586cdf93455d
agomemart <agomemart@ieschandomonte.edu.es>
2024-12-19 12:52:39 +0100

    v.4.0.0

---
+++ src/ud4/apuntes/Referencias.java
@@ -0,0 +1,8 @@
+package ud4.apuntes;
+
+import ud3.gmaexamen.CocheCarreras;
+
+public class Referencias {
+    CocheCarreras[] coche = new CocheCarreras[10];
+    
+}
---
+++ src/ud4/apuntes/TresTablas.java
@@ -0,0 +1,15 @@
+package ud4.apuntes;
+
+public class TresTablas {
+    public static void main(String[] args) {
+        int[] tabla1 = {4, 14, 55, 45, 12};
+        double[] tabla2 = {1.5, 41.25, 36.5, 15.4, 25.4};
+        boolean[] tabla3 = {true, false, true, true, false};
+
+        System.out.println(tabla1);
+        System.out.println(tabla2);
+        System.out.println(tabla3);
+
+
+    }
+}
