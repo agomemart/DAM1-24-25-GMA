3e9d7bf828549e70d495c07c6a6513864d522374
agomemart <agomemart@ieschandomonte.edu.es>
2025-01-10 21:19:12 +0100

    v.4.0.1

--- src/ud4/ejercicios/EP0512_Desordenar.java
+++ src/ud4/ejercicios/EP0512_Desordenar.java
@@ -1,12 +1,31 @@
 package ud4.ejercicios;
+
+import java.util.Arrays;
+import java.util.Random;
+
 /*
  *  Escribe la función void desordenar(int t[]), que cambia de forma 
 aleatoria los elementos contenidos en la tabla t. Si la tabla estuviera ordenada, dejaría de estarlo. 
  */
 public class EP0512_Desordenar {
     static void desordenar(int t[]) {
-        for(int i = 0; i > t.length; i++) {
-            t[i] = t[i + 2];
+        Random random = new Random();
+        
+        for(int i = t.length - 1; i > 0; i--) {
+            int j = random.nextInt(i + 1);
+            int intercambio = t[i];
+            t[i] = t[j];
+            t[j] = intercambio;
         }
+    }
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        System.out.println("Array original:");
+        System.out.println(Arrays.toString(array));
+
+        System.out.println("Array desordenado:");
+        desordenar(array);
+        System.out.println(Arrays.toString(array));
     }
 }
--- src/ud4/ejercicios/EP0514_Sueldos.java
+++ src/ud4/ejercicios/EP0514_Sueldos.java
@@ -1,5 +1,6 @@
 package ud4.ejercicios;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /*
@@ -16,7 +17,7 @@ Una vez terminada la entrada de datos, muestra la siguiente información:
  */
 public class EP0514_Sueldos {
     public static void main(String[] args) {
-        int numPersonas = 1;
+        int numPersonas = 0;
         int importeSueldo = 0;
         int t1[] = new int[numPersonas];
 
@@ -24,15 +25,59 @@ public class EP0514_Sueldos {
         do {
             System.out.print("Inserta el importe de tu sueldo: ");
             importeSueldo = sc.nextInt();
+            numPersonas++;
             if (importeSueldo != -1) {
-                for (int i = 0; i < t1.length; i++) {
-                    t1[i] = importeSueldo;
+                // Verificar que no se exceda el tamaño del array
+                if (numPersonas < t1.length) {
+                    t1[numPersonas] = importeSueldo;
                     numPersonas++;
                 }
-            } else {
-                System.out.println("Fin programa");
             }
         } while (importeSueldo != -1);
         sc.close();
+        
+        if (numPersonas == 0) {
+            System.out.println("No se ingresaron sueldos.");
+            return;
+        }
+
+        // Redimensionar el array para ajustarse al número real de sueldos ingresados
+        t1 = Arrays.copyOf(t1, numPersonas);
+
+        // Ordenar sueldos en orden decreciente
+        Arrays.sort(t1);
+        for (int i = 0; i < numPersonas / 2; i++) {
+            int temp = t1[i];
+            t1[i] = t1[numPersonas - 1 - i];
+            t1[numPersonas - 1 - i] = temp;
+        }
+
+        // Mostrar sueldos ordenados
+        System.out.println("Sueldos en orden decreciente: " + Arrays.toString(t1));
+
+        // Calcular sueldo máximo y mínimo
+        int sueldoMaximo = t1[0];
+        int sueldoMinimo = t1[numPersonas - 1];
+        System.out.println("Sueldo máximo: " + sueldoMaximo);
+        System.out.println("Sueldo mínimo: " + sueldoMinimo);
+
+        // Calcular la media de los sueldos
+        double suma = 0;
+        for (int sueldo : t1) {
+            suma += sueldo;
+        }
+        double media = suma / numPersonas;
+        System.out.println("Media de los sueldos: " + media);
+
+        // Contar sueldos por encima y por debajo de la media
+        int porEncimaMedia = 0;
+        int porDebajoMedia = 0;
+        for (int sueldo : t1) {
+            if (sueldo > media) porEncimaMedia++;
+            if (sueldo < media) porDebajoMedia++;
+        }
+
+        System.out.println("Sueldos por encima de la media: " + porEncimaMedia);
+        System.out.println("Sueldos por debajo de la media: " + porDebajoMedia);
     }
 }
