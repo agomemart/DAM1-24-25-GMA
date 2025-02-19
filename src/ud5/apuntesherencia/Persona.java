e5b51f40cf64b1ef9944e69f1c08b6c91ea5f691
agomemart <agomemart@ieschandomonte.edu.es>
2025-02-19 14:22:54 +0100

    v.5.0.1

---
+++ src/ud5/apuntesherencia/Persona.java
@@ -0,0 +1,70 @@
+package ud3.apuntes;
+
+public class Persona {
+    //Datos 
+    private String nombre;
+    private String apellido1;
+    private String apellido2;
+    private int edad;
+    private double estatura = 1.80;
+    private final String dni = null;
+    public static int contadorPersonas = 0;
+    enum Sexo {HOMBRE, MUJER, NO_BINARIO};
+    private Sexo sexo;
+    DiaDeLaSemana diaPreferido;
+    public Persona() {
+    }
+    public Persona(String nombre, Sexo sexo) {
+        this.nombre = nombre;
+        this.sexo = sexo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    //Métodos
+    public void inicializar(String n, String a1, String a2, int e){                            
+           nombre = n;
+           apellido1 = a1;
+           apellido2 = a2;
+           edad = e;
+    }
+
+    public String nombreCompleto() {
+           return nombre + " " + apellido1 + " " + apellido2;
+    }
+
+    public boolean esMayorEdad() {
+           if (edad >= 18){
+               return true;
+           }else{
+               return false;
+           }
+    }
+
+    public void cumplirAnhos() {
+        edad++;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public static void incrementarContador() {
+        contadorPersonas++;
+    }
+    public String getDni() {
+        return dni;
+    }
+    public Sexo getSexo() {
+        return sexo;
+    }
+    public double getEstatura() {
+        return estatura;
+    }
+}
---
+++ src/ud5/rol/Personaje.java
@@ -0,0 +1,4 @@
+package ud5.rol;
+
+public class Personaje {
+}
