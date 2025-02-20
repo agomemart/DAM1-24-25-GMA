9121b1f2f6e4b7e6f7e98d9cd1e3c437ff30a768
agomemart <agomemart@ieschandomonte.edu.es>
2025-02-20 12:52:52 +0100

    v.5.0.1

--- src/ud5/apuntesherencia/Hora.java
+++ src/ud5/apuntesherencia/Hora.java
@@ -1,4 +1,47 @@
 package ud5.apuntesherencia;
 
-public class E0801 {
+public class Hora {
+    int hora;
+    int minutos;
+
+    public Hora(int hora, int minutos) {
+        this.hora = hora;
+        this.minutos = minutos;
+    }
+
+    public void inc() {
+        if (minutos == 59) {
+            hora++;
+            minutos = 0;
+        } else if (hora == 24){
+            hora = 0;
+        } else {
+            minutos++;
+        }
+    }
+
+    public boolean setMinutos(int minutos) {
+        if (minutos >= 0 && minutos < 60) {
+            this.minutos = minutos;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean setHora(int hora) {
+        if (hora >= 0 && hora <= 23) {
+            this.hora = hora;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Hora{" +
+                "hora=" + hora +
+                ", minutos=" + minutos +
+                '}';
+    }
+
 }
--- src/ud5/apuntesherencia/HoraExacta.java
+++ src/ud5/apuntesherencia/HoraExacta.java
@@ -1,4 +1,31 @@
 package ud5.apuntesherencia;
 
-public class HoraExacta {
+public class HoraExacta extends Hora{
+    int segundos;
+    public HoraExacta(int hora, int minutos, int segundos) {
+        super(hora, minutos);
+        this.segundos = segundos;
+    }
+
+    public boolean setSegundos(int segundos) {
+        if (segundos >= 0 && segundos < 60) {
+            this.segundos = segundos;
+            return true;
+        }
+        return false;
+    }
+
+    public void inc() {
+        if (segundos == 60) {
+            minutos++;
+            segundos = 0;
+        } else if (minutos == 60){
+            hora++;
+            minutos = 0;
+        } else if (hora == 24){
+            hora = 0;
+        } else {
+            segundos++;
+        }
+    }
 }
