5881e87f572b8050f5d09839ac7e07169d99c6bd
agomemart <agomemart@ieschandomonte.edu.es>
2025-03-25 14:26:02 +0100

    v.5.0.5

--- src/ud6/ContenedorLista.java
+++ src/ud6/ContenedorLista.java
@@ -1,5 +1,44 @@
 package ud6;
 
-public class ContenedorLista {
+import java.util.Arrays;
 
+//E1202
+public class ContenedorLista<T> {
+    T[] objetos;
+
+    public ContenedorLista(T[] t) {
+        objetos = t;
+    }
+
+    void insertarAlPrincipio(T nuevo) {
+        objetos = Arrays.copyOf(objetos, objetos.length + 1);
+        System.arraycopy(objetos, 0, objetos, 1, objetos.length);
+        objetos[0] = nuevo;
+    }
+    void insertarAlFinal(T nuevo) {
+        objetos = Arrays.copyOf(objetos, objetos.length + 1);
+        objetos[objetos.length - 1] = nuevo;
+    }
+    T extraerDelPrincipio() {
+        return null;
+    }
+    T extraerDelFinal() {
+        return null;
+    }
+    void ordenar() {
+
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(objetos);
+    }
+
+    public static void main(String[] args) {
+        ContenedorLista<Integer> lista = new ContenedorLista<>(new Integer[0]);
+        lista.insertarAlFinal(1);
+        lista.insertarAlFinal(2);
+        lista.insertarAlFinal(3);
+        System.out.println(lista);
+    }
 }
