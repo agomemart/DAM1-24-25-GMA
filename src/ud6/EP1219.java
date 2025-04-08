88f5ffd65d5d2b701ba2c09c457068238e0eeac5
agomemart <agomemart@ieschandomonte.edu.es>
2025-04-08 14:22:02 +0200

    v.6.0.0

--- src/ud6/EP1219.java
+++ src/ud6/EP1219.java
@@ -1,4 +1,22 @@
 package ud6;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class EP1219 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String frase = sc.nextLine();
+
+        ArrayList<String> palabras = new ArrayList<>();
+
+        for (int i = 0; i < frase.length(); i++) {
+            if (frase.charAt(i) == ' ') {
+
+            }
+        }
+
+        System.out.println(palabras.toString());
+
+    }
 }
