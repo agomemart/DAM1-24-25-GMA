641bf85ba0f0e490aa5773348f702c5d9514829c
agomemart <agomemart@ieschandomonte.edu.es>
2025-05-21 08:16:52 +0200

    v.7.0.2

--- src/contornos/u5/OfertaProveedor.java
+++ src/contornos/u5/OfertaProveedor.java
@@ -1,4 +1,25 @@
 package contornos.u5;
 
 public class OfertaProveedor {
+    private Proveedor proveedor;
+    private Articulo articulo;
+    private Supermercado supermercado;
+    private double pvd;
+
+    public OfertaProveedor(Proveedor proveedor, Articulo articulo, Supermercado supermercado, double pvd) {
+        this.proveedor = proveedor;
+        this.articulo = articulo;
+        this.supermercado = supermercado;
+        this.pvd = pvd;
+    }
+
+    @Override
+    public String toString() {
+        return "OfertaProveedor{" +
+                "proveedor=" + proveedor +
+                ", articulo=" + articulo +
+                ", supermercado=" + supermercado +
+                ", pvd=" + pvd +
+                '}';
+    }
 }
--- src/contornos/u5/Proveedor.java
+++ src/contornos/u5/Proveedor.java
@@ -1,4 +1,35 @@
 package contornos.u5;
 
+import java.util.Objects;
+
 public class Proveedor {
+    private int id;
+    private String nombre;
+    private String direccion;
+
+    public Proveedor(int id, String nombre, String direccion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.direccion = direccion;
+    }
+
+    @Override
+    public String toString() {
+        return "Proveedor{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", direccion='" + direccion + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Proveedor proveedor)) return false;
+        return id == proveedor.id && Objects.equals(nombre, proveedor.nombre) && Objects.equals(direccion, proveedor.direccion);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nombre, direccion);
+    }
 }
